dffbbb38c6163dcf3a92b90720c08481e93bf8bb
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-22 09:53:05 -0400

    moves randomly

--- JonBot/JonBot.java
+++ JonBot/JonBot.java
@@ -1,7 +1,6 @@
 package leczner.jon.JonBot;
 
 import robocode.*;
-import robocode.Robot;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -17,6 +16,7 @@ public class JonBot extends AdvancedRobot
 	private ArrayList<String> friendlies = new ArrayList<>();
 	private ArrayList enemies = new ArrayList<>();
 	private byte scanDirection = 1;
+	String targetName;
 
 	/**
 	 * run: JonBot's default behavior
@@ -26,6 +26,7 @@ public class JonBot extends AdvancedRobot
 		friendlies.add("RocketDoll");
 		friendlies.add("StaticTinTin");
 		friendlies.add("Jelly");
+		friendlies.add("McKiller");
 
 		setAllColors(Color.BLACK);
 		setBulletColor(Color.BLUE);
@@ -33,16 +34,19 @@ public class JonBot extends AdvancedRobot
 		setAdjustGunForRobotTurn(true);
 		setAdjustRadarForRobotTurn(true);
 
+		Random r = new Random();
+		int randomDirection;
 
+		System.out.println("init complete");
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			setAhead(30);
-			setTurnRight(120);
 			setTurnRadarRight(360);
+			randomDirection = r.nextInt(180);
+			setTurnRight(getHeading() - randomDirection);
+			waitFor(new TurnCompleteCondition(this));
+			setAhead(1000);
+
 			execute();
-////			onWin(new WinEvent());
-//			setBack(35);
 		}
 	}
 
@@ -50,48 +54,60 @@ public class JonBot extends AdvancedRobot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
+		if (targetName != null && !e.getName().equals(targetName)) {
+			return;
+		}
+
+		if (targetName == null) {
+			targetName = e.getName();
+			System.out.println("Tracking " + targetName);
+		}
+
 		int turnDirection;
 		if (e.getBearing() >= 0) {
 			turnDirection = 1;
 		} else {
 			turnDirection = -1;
 		}
-
-		turnRight(e.getBearing());
-		ahead(e.getDistance() + 5);
+//
+//		turnRight(e.getBearing());
+//		if (e.getDistance() > 100)
+//			ahead(e.getDistance() + 5);
+//
+//		//** based on http://mark.random-article.com/robocode/basic_scanning.html
+//		setTurnRadarRight(getHeading() - getRadarHeading() + e.getBearing());
+//		scanDirection *= -1; // changes value from 1 to -1
+//		setTurnRadarRight(360 * scanDirection);
+////		execute();
+//		// ********
+//		Robot target = null;
+//		if (friendlies.contains(e.getName())) {
+//			setEventPriority("friendly", 0);
+//			setInterruptible(true);
+////			execute();
+//		} else {
+//			//** based on https://sourceforge.net/p/robocode/discussion/116459/thread/df3a124f/
+//			enemies.add(e);
+//			System.out.println("enemy found: " + e.getName());
+//			double weakestEnergy = e.getEnergy();
+//			for (int i = 0; i < enemies.size(); i++) {
+//				Robot current = (Robot)enemies.get(i);
+//				double otherEnemyEnergy = current.getEnergy();
+//				if (otherEnemyEnergy < weakestEnergy) {
+//					weakestEnergy = otherEnemyEnergy;
+//					target = current;
+//				}
+//			}
+//			if (target.equals(e)) {
+//				double absoluteBearing = getHeading() + e.getBearing();
+//				setTurnGunRight(robocode.util.Utils.normalRelativeAngle(absoluteBearing - getGunHeading()));
+//				if (getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) < 10)
+//					setFire(Math.min(400 / e.getDistance(), 3));
+////				execute();
+//			}
+//		}
+//		//********
 		scan();
-
-		//** based on http://mark.random-article.com/robocode/basic_scanning.html
-		setTurnRadarRight(getHeading() - getRadarHeading() + e.getBearing());
-		scanDirection *= -1; // changes value from 1 to -1
-		setTurnRadarRight(360 * scanDirection);
-		// ********
-		Robot target = null;
-		if (friendlies.contains(e.getName())) {
-			setEventPriority("friendly", 0);
-			setInterruptible(true);
-		} else {
-			//** based on https://sourceforge.net/p/robocode/discussion/116459/thread/df3a124f/
-			enemies.add(e);
-			double weakestEnergy = e.getEnergy();
-			for (int i = 0; i < enemies.size(); i++) {
-				Robot current = (Robot)enemies.get(i);
-				double otherEnemyEnergy = current.getEnergy();
-				if (otherEnemyEnergy < weakestEnergy) {
-					weakestEnergy = otherEnemyEnergy;
-					target = current;
-				}
-			}
-			if (target.equals(e)) {
-				double absoluteBearing = getHeading() + e.getBearing();
-				setTurnGunRight(robocode.util.Utils.normalRelativeAngle(absoluteBearing - getGunHeading()));
-				if (getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) < 10)
-					setFire(Math.min(400 / e.getDistance(), 3));
-				execute();
-			}
-		}
-		//********
-
 	}
 
 	/**
@@ -113,9 +129,6 @@ public class JonBot extends AdvancedRobot
 		// Replace the next line with any behavior you would like
 		setTurnRight(e.getBearing());
 		setTurnGunLeft(e.getBearing());
-		Random r = new Random();
-		int randomDirection = r.nextInt((int)e.getBearing() * -1);
-		setTurnLeft(randomDirection);
 		setBack(100);
 	}	
 }
